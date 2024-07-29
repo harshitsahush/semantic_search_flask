1ce004a6dbf2e466479507fc03e15dd6e1c3cf2c
harshitsahush <harshitsahu.sh@gmail.com>
2024-07-29 15:36:38 +0530

    Added throttling and implemented basic CSS

--- static/main.js
+++ static/main.js
@@ -1,4 +1,40 @@
 $(document).ready(function(){
+    //function for throttling
+    //https://dev.to/jeetvora331/throttling-in-javascript-easiest-explanation-1081
+    function throttle(mainfn, delay)
+    {
+        let timerflag = null;
+        return (...args) => {
+            if(timerflag === null)
+            {
+                mainfn(...args);
+                timerflag = setTimeout(() => {
+                    timerflag = null;
+                }, delay);
+            }
+        };
+    }
+
+    // calls API and updates data
+    function fetchdata(){
+        const temp = document.getElementById("query_box").value;
+        $.ajax({
+            url:"/sem_search",
+            type : "GET",
+            contentType : "application/json",
+            data : {query_text : temp},
+            success : function(response){
+                document.getElementById("response").innerHTML = response;
+                console.log("Got response");
+            },
+            error : function(error){
+                console.log("Error", error);
+            }
+        });
+    }
+
+    const throttle_fetchdata = throttle(fetchdata, 500);
+
     document.getElementById("query_box").addEventListener("keyup", function(){
         const temp = document.getElementById("query_box").value;
         console.log(temp)
@@ -9,19 +45,7 @@ $(document).ready(function(){
         }
         else
         {
-            $.ajax({
-                url:"/sem_search",
-                type : "GET",
-                contentType : "application/json",
-                data : {query_text : temp},
-                success : function(response){
-                    document.getElementById("response").innerHTML = response;
-                    console.log("Got response");
-                },
-                error : function(error){
-                    console.log("Error", error);
-                }
-            });
+            throttle_fetchdata()
         }
     });
 
